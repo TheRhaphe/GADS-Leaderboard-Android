7abca595326c65eb6d87ce58883052c940cd4d97
Brighton Mahatchi <brighton.mahatchi@cassavasmartech.co.zw>
2020-09-10 13:36:29 +0200

    Added splash screen

---
+++ app/src/main/java/com/bmahatchi/gadsleaderboard/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.bmahatchi.gadsleaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new CountDownTimer(2000, 2000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            }
+        }.start();
+    }
+}
