b87175723ad7aec6f5514762360d119592abe5ef
Brighton Mahatchi <brighton.mahatchi@cassavasmartech.co.zw>
2020-09-10 16:23:00 +0200

    google forms integration

---
+++ app/src/main/java/com/bmahatchi/gadsleaderboard/GoogleFormsService.java
@@ -0,0 +1,15 @@
+package com.bmahatchi.gadsleaderboard;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface GoogleFormsService {
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @FormUrlEncoded
+    Call<Void> submitForm(@Field("entry.1824927963") String email,
+                          @Field("entry.1877115667") String firstName,
+                          @Field("entry.2006916086") String lastName,
+                          @Field("entry.284483984") String link);
+}
--- app/src/main/java/com/bmahatchi/gadsleaderboard/ProjectSubmissionActivity.java
+++ app/src/main/java/com/bmahatchi/gadsleaderboard/ProjectSubmissionActivity.java
@@ -1,8 +1,16 @@
 package com.bmahatchi.gadsleaderboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProjectSubmissionActivity extends AppCompatActivity {
 
@@ -15,5 +23,35 @@ public class ProjectSubmissionActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
             getSupportActionBar().setDisplayUseLogoEnabled(true);
         }
+
+        EditText email = findViewById(R.id.email);
+        EditText firstName = findViewById(R.id.firstName);
+        EditText lastName = findViewById(R.id.lastName);
+        EditText link = findViewById(R.id.link);
+
+        findViewById(R.id.buttonSubmit).setOnClickListener(v->submit(
+                email.getText().toString(), firstName.getText().toString(), lastName.getText().toString(),
+                link.getText().toString()
+        ));
+    }
+
+    private void submit(String email, String firstName, String lastName, String link) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://docs.google.com/forms/d/e/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GoogleFormsService service = retrofit.create(GoogleFormsService.class);
+        service.submitForm(email, firstName, lastName, link)
+                .enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+
+            }
+        });
     }
 }
