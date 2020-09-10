de7ab3e460d23a423b264eb77d56a6374ca87fbd
Brighton Mahatchi <brighton.mahatchi@cassavasmartech.co.zw>
2020-09-10 17:08:14 +0200

    Success/fail dialogs

--- app/src/main/java/com/bmahatchi/gadsleaderboard/ProjectSubmissionActivity.java
+++ app/src/main/java/com/bmahatchi/gadsleaderboard/ProjectSubmissionActivity.java
@@ -1,10 +1,16 @@
 package com.bmahatchi.gadsleaderboard;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,6 +19,13 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProjectSubmissionActivity extends AppCompatActivity {
+
+    public EditText email;
+    public EditText firstName;
+    public EditText lastName;
+    public EditText link;
+    public Button submit;
+    public ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +37,27 @@ public class ProjectSubmissionActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayUseLogoEnabled(true);
         }
 
-        EditText email = findViewById(R.id.email);
-        EditText firstName = findViewById(R.id.firstName);
-        EditText lastName = findViewById(R.id.lastName);
-        EditText link = findViewById(R.id.link);
+        email = findViewById(R.id.email);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        link = findViewById(R.id.link);
+        progressBar = findViewById(R.id.progressBar);
 
-        findViewById(R.id.buttonSubmit).setOnClickListener(v->submit(
+        submit = findViewById(R.id.buttonSubmit);
+        submit.setOnClickListener(v->submit(
                 email.getText().toString(), firstName.getText().toString(), lastName.getText().toString(),
                 link.getText().toString()
         ));
     }
 
     private void submit(String email, String firstName, String lastName, String link) {
+        if (true){
+            hideViews();
+            showFailureDialog();
+            return;
+        }
+        hideViews();
+        progressBar.setVisibility(View.VISIBLE);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://docs.google.com/forms/d/e/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -45,13 +67,42 @@ public class ProjectSubmissionActivity extends AppCompatActivity {
                 .enqueue(new Callback<Void>() {
             @Override
             public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
-
+                progressBar.setVisibility(View.GONE);
+                showSuccessDialog();
             }
 
             @Override
             public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
-
+                progressBar.setVisibility(View.GONE);
+                showFailureDialog();
             }
         });
+    }
+    private void showSuccessDialog() {
+        new MaterialAlertDialogBuilder(this, R.style.MaterialAlertDialog_rounded)
+                .setView(R.layout.dialog_success)
+                .show();
+    }
+
+    private void showFailureDialog() {
+        new MaterialAlertDialogBuilder(this, R.style.MaterialAlertDialog_rounded)
+                .setView(R.layout.dialog_failure)
+                .show();
+    }
+
+    private void hideViews() {
+        email.setVisibility(View.INVISIBLE);
+        firstName.setVisibility(View.INVISIBLE);
+        lastName.setVisibility(View.INVISIBLE);
+        link.setVisibility(View.INVISIBLE);
+        submit.setVisibility(View.INVISIBLE);
+    }
+
+    private void showViews() {
+        email.setVisibility(View.VISIBLE);
+        firstName.setVisibility(View.VISIBLE);
+        lastName.setVisibility(View.VISIBLE);
+        link.setVisibility(View.VISIBLE);
+        submit.setVisibility(View.VISIBLE);
     }
 }
